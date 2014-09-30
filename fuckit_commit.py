69c6fac162def2dd22ff32fb69dae5dd8ba7a4cd
ueg1990 <uehtesham90@gmail.com>
2014-09-30 17:53:28 -0400

    Add docstrings

--- fuckit_commit.py
+++ fuckit_commit.py
@@ -6,12 +6,21 @@ The aim is to encourage user to code, commit and push to GitHub everyday
 import requests
 
 def set_configuration():
+    '''
+    Set Twilio configuration
+    '''
     pass
 
 def get_twilio_client():
+    '''
+    Connect to Twilio Client
+    '''
     pass
 
 def send_sms():
+    '''
+    Send SMS reminder
+    '''
     pass
 
 def main():
