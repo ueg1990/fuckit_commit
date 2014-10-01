36b771ce3028200f57255633dbfa4f6b991e1674
ueg1990 <uehtesham90@gmail.com>
2014-10-01 05:29:03 -0400

    Add code to send sms

--- fuckit_commit.py
+++ fuckit_commit.py
@@ -6,28 +6,17 @@ The aim is to encourage user to code, commit and push to GitHub everyday
 import requests
 from twilio.rest import TwilioRestClient
 
-def get_configuration():
-    '''
-    Set Twilio configuration
-    '''
-    pass
-
-def get_twilio_client(config):
-    '''
-    Connect to Twilio Client
-    '''
-    return TwilioRestClient(config.account_sid, config.auth_token)
-
-def send_sms(client):
+def send_sms():
     '''
     Send SMS reminder
     '''
-    pass
+    config = {'account_sid' : '', 'auth_token' : ''}
+    client = TwilioRestClient(config['account_sid'], config['auth_token'])
+    message = client.messages.create(to="+15148871900", from_="+14387937890",
+                                     body="Hello there!")
 
 def main():
-    config = get_configuration()
-    client = get_configuration(config)
-    send_sms(client)
+    send_sms()
 
 if __name__ == "__main__":
     main()
