15463168ed715761eaf483a1e53eb74d92b83e04
ueg1990 <uehtesham90@gmail.com>
2014-10-03 03:09:31 -0400

    Add test to send sms

--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
 import unittest
+import json
+from twilio.rest import TwilioRestClient
+
 import fuckit_commit
 
 class Fuckit_CommitTestCase(unittest.TestCase):
@@ -6,10 +9,14 @@ class Fuckit_CommitTestCase(unittest.TestCase):
     Unit Test cases for fuckit_commit
     '''
     def setUp(self):
-	pass
+	with open('configuration.json') as f:
+	    self.config = json.load(f)
 
     def test_send_sms(self):
-        pass
+	client = TwilioRestClient(self.config['twilio']['sid'], self.config['twilio']['auth_token'])
+        message = client.messages.create(to=self.config['twilio']["number_to"], from_=self.config['twilio']["number_from"],
+                                     body="You need to commit today!!!\nFuck it!!! Commit!!!")        
+	self.assertEqual(message.account_sid, self.config['twilio']['sid'])
 
     def tearDown(self):
 	pass
