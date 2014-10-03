a6219932c07c01e31748ce618aa9872e2393740a
ueg1990 <uehtesham90@gmail.com>
2014-10-03 00:57:31 -0400

    Add config info and test if sms is sent if no github activity

--- fuckit_commit.py
+++ fuckit_commit.py
@@ -8,31 +8,32 @@ from twilio.rest import TwilioRestClient
 from datetime import datetime, date
 import json
 
-def send_sms():
+def get_config_info():
+    with open('configuration2.json') as f:
+	data = json.load(f)
+    return data
+
+def send_sms(twilio_info):
     '''
     Send SMS reminder
     '''
-    config = {'account_sid' : '', 'auth_token' : ''}
-    client = TwilioRestClient(config['account_sid'], config['auth_token'])
-    message = client.messages.create(to="", from_="",
-                                     body="Hello there!")
-def check_commit_activity():
+    client = TwilioRestClient(twilio_info['sid'], twilio_info['auth_token'])
+    message = client.messages.create(to=twilio_info["number_to"], from_=twilio_info["number_from"],
+                                     body="You need to commit today!!!\nFuck it!!! Commit!!!")
+def check_commit_activity(github_info):
     '''
     Check if there was any change in the commit history of the user. If date of 
     latest event is the same as current date, commit was made, hence return True
     else return False. Returning False triggers sending of an SMS reminder
     '''
-    get_recent_event = requests.get("https://api.github.com/users/%s/events/public" % '')
+    get_recent_event = requests.get("https://api.github.com/users/%s/events/public" % github_info['username'])
     event_date =  datetime.strptime(get_recent_event.json()[0]['created_at'].split('T')[0], '%Y-%m-%d').date()
     return event_date == date.today()
 
 def main():
-    with open('configuration.json') as f:
-	data = json.loads(f)
-    print data
-    print type(data)
-    #if not check_commit_activity():
-    #    send_sms()
+    config = get_config_info() 
+    if not check_commit_activity(config['github']):
+        send_sms(config['twilio'])
 
 if __name__ == "__main__":
     main()
